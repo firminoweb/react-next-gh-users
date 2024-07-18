04537a74bf24fd7b88be73f93d446a6bbe152879
João Firmino <firminoata@gmail.com>
2024-07-18 11:21:22 -0300

    feat: fixing nextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  },
 };
   
 module.exports = nextConfig;
